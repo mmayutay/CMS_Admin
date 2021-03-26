546c0dad4bf95ed303be4b11da8dda1789904551
JessaMaeYosores <jessamae.yosores@student.passerellesnumeriques.org>
2021-03-26 09:48:50 +0800

    LogIn Page

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -14,6 +14,7 @@ import { MapsComponent }            from '../../pages/maps/maps.component';
 import { NotificationsComponent }   from '../../pages/notifications/notifications.component';
 import { UpgradeComponent }         from '../../pages/upgrade/upgrade.component';
 
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
